d898d6fd12c2f7ad27a5075f35f3ba61b85cab32
Lucas Rodrigues <lucasrodrigues123@hotmail.com.br>
2021-04-18 19:16:19 -0300

    Add paths to be ignored by coverage testing

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,11 @@ module.exports = {
   clearMocks: true,
   collectCoverageFrom: ['src/**'],
   coverageDirectory: '__tests__/coverage',
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/infra/database/migrations/',
+    '/*.json'
+  ],
   coverageProvider: 'v8',
   setupFiles: ['dotenv/config'],
   setupFilesAfterEnv: ['./__tests__/index'],
