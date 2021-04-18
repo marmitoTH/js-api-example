ca308da171631ccf616fba260b80608774ab4eb8
Lucas Rodrigues <lucasrodrigues123@hotmail.com.br>
2021-04-18 19:43:51 -0300

    Fix migrations being generated in the wrong directory

--- src/infra/database/options.js
+++ src/infra/database/options.js
@@ -1,15 +1,13 @@
-const path = require('path')
-
 const IN_TEST = process.env.NODE_ENV === 'test'
 
 module.exports = {
   synchronize: IN_TEST,
   type: process.env.DB_TYPE,
   url: IN_TEST ? process.env.DB_TEST_URL : process.env.DB_URL,
-  entities: [path.join(__dirname, 'models/**/*.js')],
-  migrations: [path.join(__dirname, 'migrations/**/*.js')],
+  entities: ['src/infra/database/models/**/*.js'],
+  migrations: ['src/infra/database/migrations/**/*.js'],
   cli: {
-    entitiesDir: path.join(__dirname, 'models'),
-    migrationsDir: path.join(__dirname, 'migrations')
+    entitiesDir: 'src/infra/database/models',
+    migrationsDir: 'src/infra/database/migrations'
   }
 }
